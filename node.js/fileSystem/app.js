a32648c732194f983a130eeb45469ac6d18fe723
gunesoguzhan <gunesoguzhan@outlook.com.tr>
2022-10-02 09:25:31 +0300

    nodejs server

--- node.js/fileSystem/app.js
+++ node.js/fileSystem/app.js
@@ -3,4 +3,4 @@ const fileOperations = require('./fileOperations.js');
 fileOperations.writeFile('./employee.json', '{"name": "Employee 1 Name", "salary": 2000}');
 fileOperations.appendFile('./employee.json', '{"name": "Employee 2 Name", "salary": 2300}');
 fileOperations.readFile('./employee.json');
-// fileOperations.deleteFile('./employee.json');
+fileOperations.deleteFile('./employee.json');
